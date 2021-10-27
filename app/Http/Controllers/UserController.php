96185fcec7feed72e2277782388128df4f866b72
itsgitz <anggit.ginanjar@outlook.com>
2021-10-27 23:37:52 +0700

    feat(user): Add view and controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('user.index', [
+            'users' => User::all()
+        ]);
+    }
+
+    public function show($id)
+    {
+        return view('user.show', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Lists</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Users List:</h1> 
+        @foreach ($users as $u)
+        <p>{{ $u->name }} | {{ $u->email }}</p>
+        @endforeach
+    </div>
+</body>
+</html>
