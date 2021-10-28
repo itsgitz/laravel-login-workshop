8f7865e0ca7c680831bcafd76147f74439885b1d
itsgitz <anggit.ginanjar@outlook.com>
2021-10-28 14:15:21 +0700

    feat: Add controller and views for home and login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+//use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function auth(Request $r)
+    {
+        $credentials = $r->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $r->session()->regenerate();
+
+            return redirect()->intended('users');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credential: email'
+        ]);
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Login page</h1>
+        <form method="post" action="{{ route('login') }}">
+            @csrf
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password">
+            </div>
+            <div>
+                <input type="submit" value="login">
+            </div>
+        </form>
+        <div>
+            <a href="{{ route('home') }}">Home</a>
+        </div>
+    </div> 
+</body>
+</html>
