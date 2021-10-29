fb5c214d2f03cd1cd528c0a0c67782cf1cb97aea
itsgitz <anggit.ginanjar@outlook.com>
2021-10-29 23:20:27 +0700

    feat(blade): Add logout button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,17 @@
     <title>Home page!</title>
 </head>
 <body>
-   <div class="container">
-    <h1>Hello from home!</h1>
-    <a href="{{ route('users') }}">View list of users</a>
-   </div> 
+    <div class="container">
+        <h1>Hello from home!</h1>
+        <a href="{{ route('users') }}">View list of users</a>
+    </div> 
+    @if (Auth::check())
+    <div>
+        <form method="post" action="{{ route('logout') }}">
+            @csrf
+            <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit()">Logout</a>
+        </form>
+    </div>
+    @endif
 </body>
 </html>
