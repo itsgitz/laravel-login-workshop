e23a2feb63427516893c8336cf4f6e36f80d2d6c
itsgitz <anggit.ginanjar@outlook.com>
2021-10-31 14:35:04 +0700

    chore: Removed middleware from construct function. Use on route instead.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,6 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         return view('user.index', [
